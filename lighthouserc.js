d5afc3c91aa3a98313badba621e69ae86051a903
bekapod <beckyjns@gmail.com>
2022-02-06 14:59:18 +0000

    👷 add lhci

---
+++ lighthouserc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ["http://localhost:3000/"],
+      startServerCommand: "npm start",
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+    },
+    upload: {
+      target: "lhci",
+    },
+  },
+};
